f829d6e963136f22324f7bb2a0fdcc1db223acb0
노승영 <syby1422@naver.com>
2021-11-05 17:37:23 +0900

    스크롤 처리

--- app/src/main/java/com/choonoh/soobook/Sign_up.java
+++ app/src/main/java/com/choonoh/soobook/Sign_up.java
@@ -1,5 +1,6 @@
 package com.choonoh.soobook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,6 +12,9 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -26,12 +30,13 @@ import java.util.Map;
 public class Sign_up extends AppCompatActivity {
 
     private EditText et_email, et_pwd, et_pwd_check, et_nick;
-    private Button only_one_btn, sign_up_btn;
+    private Button sign_up_btn;
     private ImageButton back_btn;
     private FirebaseAuth firebaseAuth;
     private FirebaseUser currentUser;
     private String state="상태메시지를 입력해주세요";
     private String pic="none";
+    private String user_UID, user_email, user_nick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,8 +51,8 @@ public class Sign_up extends AppCompatActivity {
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(intent);
         });
-
     }
+
     public void setID() {
         et_email = findViewById(R.id.et_email);
         et_pwd = findViewById(R.id.et_pwd);
@@ -56,6 +61,7 @@ public class Sign_up extends AppCompatActivity {
         sign_up_btn = findViewById(R.id.sign_up_btn);
         firebaseAuth = FirebaseAuth.getInstance();
     }
+
     public void setEvents() {
         sign_up_btn.setOnClickListener(v -> {
             Toast toast;
@@ -76,81 +82,60 @@ public class Sign_up extends AppCompatActivity {
     }
 
     public void postFirebaseDatabase(boolean add){
-        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
-        String user_UID = currentUser.getUid();
-        String user_email= currentUser.getEmail();
-        String user_nick = et_nick.getText().toString();
-
-
+        currentUser = firebaseAuth.getCurrentUser();
+        //user_UID = currentUser.getUid();
+        user_email= currentUser.getEmail();
+        user_nick = et_nick.getText().toString();
     }
 
-
     public void startSignUp() {
-        firebaseAuth.createUserWithEmailAndPassword(et_email.getText().toString(), et_pwd.getText().toString()).addOnCompleteListener(this, task -> {
-            postFirebaseDatabase(true);
-            Toast toast;
-            Handler handler = new Handler();
-            if (task.isSuccessful()) {
-                FirebaseUser currentUser = firebaseAuth.getCurrentUser();
-                String user_UID = currentUser.getUid();
-                String user_email= currentUser.getEmail();
-                String user_nick = et_nick.getText().toString();
-                //db에 user 추가
-                HashMap<Object,String> hashMap = new HashMap<>();
+        firebaseAuth.createUserWithEmailAndPassword((et_email.getText().toString()), et_pwd.getText().toString()).addOnCompleteListener(Sign_up.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Toast toast;
+                Handler handler = new Handler();
+                if (task.isSuccessful()) {
+                    FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+                    user_UID = currentUser.getUid();
+                    user_email= currentUser.getEmail();
+                    user_nick = et_nick.getText().toString();
+                    //db에 user 추가
+                    HashMap<Object,String> hashMap = new HashMap<>();
 
-                hashMap.put("uid",user_UID);
-                hashMap.put("email",user_email);
-                hashMap.put("nick",user_nick);
-                hashMap.put("state", "상태메세지를 입력해주세요");
+                    hashMap.put("uid",user_UID);
+                    hashMap.put("email",user_email);
+                    hashMap.put("nick",user_nick);
+                    hashMap.put("state", "상태메세지를 입력해주세요");
 
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference reference = database.getReference("User");
-                reference.child(user_UID).setValue(hashMap);
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference reference = database.getReference("User");
+                    reference.child(user_UID).setValue(hashMap);
 
-                HashMap<Object,String> hashMap2 = new HashMap<>();
-                hashMap2.put("totalBookNum", Integer.toString(0));
-                hashMap2.put("totalReadBookNum", Integer.toString(0));
+                    HashMap<Object,String> hashMap2 = new HashMap<>();
+                    hashMap2.put("totalBookNum", Integer.toString(0));
+                    hashMap2.put("totalReadBookNum", Integer.toString(0));
 
-                hashMap2.put("monday", Integer.toString(0));
-                hashMap2.put("tuesday", Integer.toString(0));
-                hashMap2.put("wednesday", Integer.toString(0));
-                hashMap2.put("thursday", Integer.toString(0));
-                hashMap2.put("friday", Integer.toString(0));
-                hashMap2.put("saturday", Integer.toString(0));
-                hashMap2.put("sunday", Integer.toString(0));
+                    hashMap2.put("monday", Integer.toString(0));
+                    hashMap2.put("tuesday", Integer.toString(0));
+                    hashMap2.put("wednesday", Integer.toString(0));
+                    hashMap2.put("thursday", Integer.toString(0));
+                    hashMap2.put("friday", Integer.toString(0));
+                    hashMap2.put("saturday", Integer.toString(0));
+                    hashMap2.put("sunday", Integer.toString(0));
 
-                reference = database.getReference("ReadTime/info");
-                reference.child(user_UID).setValue(hashMap2);
-/*
-            DatabaseReference hopperRef = database.getReference("ReadTime/0ABGKRMonqbw6Pbx3aASBJvxyEa2/").child("info");
-            Map<String, Object> hopperUpdates = new HashMap<>();
-            hopperUpdates.put("readBookNum", plusOne);
+                    reference = database.getReference("ReadTime/info");
+                    reference.child(user_UID).setValue(hashMap2);
 
-            hopperRef.updateChildren(hopperUpdates);
-            */
-
-
-/*
-            Map<String, Object> childUpdates = new HashMap<>();
-            Map<String, Object> postValues = null;
-            if(true){
-                FirebaseuserPost post = new FirebaseuserPost(user_email, user_UID, user_nick, state, pic);
-                postValues = post.toMap();
-            }
-            String root ="/User/"+user_UID;
-            childUpdates.put(root, postValues);
-            mPostReference.updateChildren(childUpdates);
-*/
-               // firebaseAuth.signOut();
-                Intent intent = new Intent(Sign_up.this, Login.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                startActivity(intent);
-                toast = Toast.makeText(Sign_up.this, "수북의 회원이 되신 것을 환영합니다!", Toast.LENGTH_SHORT); toast.show();
-                handler.postDelayed(toast::cancel, 1000);
-                finish();
-            } else {
-                toast = Toast.makeText(Sign_up.this, "이메일,비밀번호 형식(영문, 숫자, 특수문자)을 확인하고 다시 시도해주세요.", Toast.LENGTH_SHORT); toast.show();
-                handler.postDelayed(toast::cancel, 1000);
+                    Intent intent = new Intent(Sign_up.this, Login.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                    startActivity(intent);
+                    toast = Toast.makeText(Sign_up.this, "수북의 회원이 되신 것을 환영합니다!", Toast.LENGTH_SHORT); toast.show();
+                    handler.postDelayed(toast::cancel, 1000);
+                    finish();
+                } else {
+                    toast = Toast.makeText(Sign_up.this, "이메일,비밀번호 형식(영문, 숫자, 특수문자)을 확인하고 다시 시도해주세요.", Toast.LENGTH_SHORT); toast.show();
+                    handler.postDelayed(toast::cancel, 1000);
+                }
             }
         });
     }
